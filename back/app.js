2e01b11fc8a52428036325f8114add49102bcbd4
Sahar Moshe <saharm123456@gmail.com>
2023-06-08 20:53:40 +0300

    skeleton

---
+++ back/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const cors = require('cors');
+const handleMainError = require('./middleware/handleMainError');
+
+const router = require('./routes/router');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(helmet());
+app.use(cors());
+app.options('*', cors());
+app.use(router);
+app.use(handleMainError);
+
+const { PORT = 3001 } = process.env;
+app.listen(PORT);
+ 
---
+++ back/middleware/handleMainError.js
@@ -0,0 +1,6 @@
+const handleMainError = (err, req, res, next) => {
+    console.log(err);
+    res.status(500).send('Internal Server Error');
+    }
+
+module.exports = handleMainError;
