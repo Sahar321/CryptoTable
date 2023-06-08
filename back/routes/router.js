79df33736ea51b383c0391107d2ffe9f4682f12b
Sahar Moshe <saharm123456@gmail.com>
2023-06-09 01:01:22 +0300

    fix route

--- back/routes/router.js
+++ back/routes/router.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const cryptoTokens = require('./crypto-tokens');
 
-router.all('/crypto-tokens', cryptoTokens);
+router.all('/crypto-tokens*', cryptoTokens);
 
 router.all('*', (req, res, next) => {
   res.send('not found');
