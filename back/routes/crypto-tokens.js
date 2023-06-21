912f8323d9ee738471355a3dd8185e86a4fec7a1
Sahar <saharm123456@gmail.com>
2023-06-22 01:35:40 +0300

    change variable name

--- back/routes/crypto-tokens.js
+++ back/routes/crypto-tokens.js
@@ -4,7 +4,7 @@ const {
   getTokenById,
 } = require('../controllers/crypto-tokens');
 router.get('/crypto-tokens', getTokenData);
-router.get('/crypto-tokens/:tokenId', getTokenById);
+router.get('/crypto-tokens/:id', getTokenById);
 /* router.get('/crypto-tokens', (req, res, next) => {
   res.send({ tokens: extractedData });
 }); */
