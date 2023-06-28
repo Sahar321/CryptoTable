2066964dc8c6f0e03e69826508bef2adc9d5996a
Sahar <saharm123456@gmail.com>
2023-06-28 13:13:29 +0300

    add default values

--- back/middleware/handleMainError.js
+++ back/middleware/handleMainError.js
@@ -1,6 +1,5 @@
 const handleMainError = (err, req, res, next) => {
-    console.log(err);
-    res.status(500).send('Internal Server Error');
-    }
-    
-module.exports = handleMainError
+  const { statusCode = 500, message = 'Internal Server Error' } = err;
+  res.status(statusCode).send(message);
+};
+module.exports = handleMainError;
